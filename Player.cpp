73c9db4c92325d8a05c81bdac3c6970beb2bb5dc
BabaIbuki <k024g0056@g.neec.ac.jp>
2024-12-17 23:42:28 +0900

    治った！

--- Player.cpp
+++ Player.cpp
@@ -54,32 +54,32 @@ void PlayerInitialize(GameObject* go) {
 //プレイヤーの動きの関数（マップチップとの当たり判定も込みの可能性もあり）
 void PlayerMove(GameObject* go, Key* key) {
 
-	/*if (!go->buddy.IsClear) {
+	if (!go->buddy.IsClear) {
 		if (!go->player.MoveFlag) {
 			
 			go->player.virtualPos = go->player.Pos;
 			go->player.PrePos = go->player.Pos;
 
 			if (key->keys[DIK_W]) {
-				go->player.virtualPos.y -= float(BLOCKSIZE)*2;
+				go->player.virtualPos.y -= static_cast<float>(BLOCKSIZE);
 			}
 			else if (key->keys[DIK_S]) {
-				go->player.virtualPos.y += float(BLOCKSIZE)*2;
+				go->player.virtualPos.y += static_cast<float>(BLOCKSIZE);
 			}
 			else if (key->keys[DIK_A]) {
-				go->player.virtualPos.x -= float(BLOCKSIZE)*2;
+				go->player.virtualPos.x -= static_cast<float>(BLOCKSIZE);
 				if (go->player.direction != LEFT) {
 					go->player.direction = LEFT;
 				}
 			}
 			else if (key->keys[DIK_D]) {
-				go->player.virtualPos.x += float(BLOCKSIZE)*2;
+				go->player.virtualPos.x += static_cast<float>(BLOCKSIZE);
 				if (go->player.direction != RIGHT) {
 					go->player.direction = RIGHT;
 				}
 			}
 		}
-		if (go->player.virtualPos.x!= go->player.Pos.x|| go->player.virtualPos.y != go->player.Pos.y) {
+		if (go->player.virtualPos.x != go->player.Pos.x || go->player.virtualPos.y != go->player.Pos.y) {
 			if (go->MapChip[int(go->player.virtualPos.y) / BLOCKSIZE][int(go->player.virtualPos.x) / BLOCKSIZE].Map != 1) {
 				go->player.MoveFlag = true;
 			}
@@ -89,59 +89,59 @@ void PlayerMove(GameObject* go, Key* key) {
 		}
 		if (go->player.MoveFlag) {
 			Vector2 Move;
-			Move.x = go->player.virtualPos.x - go->player.Pos.x;
-			Move.y = go->player.virtualPos.y - go->player.Pos.y;
-			go->player.Pos.x = static_cast<float>(EASE::InOutQuad(Move.x, go->player.PrePos.x, 10, go->player.MoveCoolTime));
-			go->player.Pos.y = static_cast<float>(EASE::InOutQuad(Move.y, go->player.PrePos.y, 10, go->player.MoveCoolTime));
+			Move.x = go->player.virtualPos.x - go->player.PrePos.x;
+			Move.y = go->player.virtualPos.y - go->player.PrePos.y;
+			go->player.Pos.x = static_cast<float>(EASE::InOutQuad(static_cast<double>(Move.x), static_cast<double>(go->player.PrePos.x), 10, static_cast<double>(go->player.MoveCoolTime)));
+			go->player.Pos.y = static_cast<float>(EASE::InOutQuad(static_cast<double>(Move.y), static_cast<double>(go->player.PrePos.y), static_cast<double>(10), static_cast<double>(go->player.MoveCoolTime)));
 			go->player.MoveCoolTime++;
 		}
 
-		if (go->player.MoveCoolTime == 10) {
+		if (go->player.MoveCoolTime > 10) {
 			go->player.MoveCoolTime = 0;
 			go->player.MoveFlag = false;
 		}
 		Novice::ScreenPrintf(20, 20, " % f", go->player.virtualPos.x);
 		Novice::ScreenPrintf(20, 40, " % f", go->player.Pos.x);
-	}*/
+	}
 
 	
 
-		go->player.PrePos = go->player.Pos;
+	//	go->player.PrePos = go->player.Pos;
 
-	if (!go->buddy.IsClear) {
-		if (go->player.MoveCoolTime == 10) {
-			if (key->keys[DIK_W]) {
-				go->player.Pos.y -= BLOCKSIZE;
-				go->player.MoveFlag = true;
-			}
-			else if (key->keys[DIK_S]) {
-				go->player.Pos.y += BLOCKSIZE;
-				go->player.MoveFlag = true;
-			}
-			else if (key->keys[DIK_A]) {
-				go->player.Pos.x -= BLOCKSIZE;
-				go->player.MoveFlag = true;
-			}
-			else if (key->keys[DIK_D]) {
-				go->player.Pos.x += BLOCKSIZE;
-				go->player.MoveFlag = true;
-			}
-		}
-	}
+	//if (!go->buddy.IsClear) {
+	//	if (go->player.MoveCoolTime == 10) {
+	//		if (key->keys[DIK_W]) {
+	//			go->player.Pos.y -= BLOCKSIZE;
+	//			go->player.MoveFlag = true;
+	//		}
+	//		else if (key->keys[DIK_S]) {
+	//			go->player.Pos.y += BLOCKSIZE;
+	//			go->player.MoveFlag = true;
+	//		}
+	//		else if (key->keys[DIK_A]) {
+	//			go->player.Pos.x -= BLOCKSIZE;
+	//			go->player.MoveFlag = true;
+	//		}
+	//		else if (key->keys[DIK_D]) {
+	//			go->player.Pos.x += BLOCKSIZE;
+	//			go->player.MoveFlag = true;
+	//		}
+	//	}
+	//}
 
-	if (go->player.MoveFlag) {
-		go->player.MoveCoolTime--;
-	}
-	if (go->player.MoveCoolTime <= 0) {
-		go->player.MoveCoolTime = 10;
-		go->player.MoveFlag = false;
-	}
+	//if (go->player.MoveFlag) {
+	//	go->player.MoveCoolTime--;
+	//}
+	//if (go->player.MoveCoolTime <= 0) {
+	//	go->player.MoveCoolTime = 10;
+	//	go->player.MoveFlag = false;
+	//}
 
-	if (go->MapChip[int(go->player.Pos.y) / BLOCKSIZE][int(go->player.Pos.x) / BLOCKSIZE].Map == 1
-		//|| go->MapChip[int(go->player.Pos.y) / BLOCKSIZE][int(go->player.Pos.x) / BLOCKSIZE].Map == 2
-		/* || go->MapChip[int(go->player.Pos.y) / BLOCKSIZE][int(go->player.Pos.x) / BLOCKSIZE].Map == 3*/) {
-		go->player.Pos = go->player.PrePos;
-	}
+	//if (go->MapChip[int(go->player.Pos.y) / BLOCKSIZE][int(go->player.Pos.x) / BLOCKSIZE].Map == 1
+	//	//|| go->MapChip[int(go->player.Pos.y) / BLOCKSIZE][int(go->player.Pos.x) / BLOCKSIZE].Map == 2
+	//	/* || go->MapChip[int(go->player.Pos.y) / BLOCKSIZE][int(go->player.Pos.x) / BLOCKSIZE].Map == 3*/) {
+	//	go->player.Pos = go->player.PrePos;
+	//}
 
 }
 
